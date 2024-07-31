7eaaca3f3e61023798355662efabb1194f7b1b87
euler <kim25444@naver.com>
2024-07-31 14:04:37 +0900

    o modify: create backup for bashrc/zshrc file

--- install.sh
+++ install.sh
@@ -4,8 +4,14 @@ TOP_DIR=$(cd $(dirname "$0") && pwd);
 CURRENT_SHELL=$(echo $SHELL | awk -F '/' '{print $NF}');
 
 if [ $CURRENT_SHELL = "bash" ]; then
+	if [ -f $HOME/.bashrc ]; then
+		mv $HOME/.bashrc $HOME/.bashrc.$(date +%Y%m%d%H%M%S);
+	fi;
 	ln -s $TOP_DIR/.core.sh $HOME/.bashrc;
 elif [ $CURRENT_SHELL = "zsh" ]; then
+	if [ -f $HOME/.zshrc ]; then
+		mv $HOME/.zshrc $HOME/.zshrc.$(date +%Y%m%d%H%M%S);
+	fi;
 	ln -s $TOP_DIR/.core.sh $HOME/.zshrc;
 else
 	echo "Unsupported shell: $CURRENT_SHELL";
