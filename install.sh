55476321b727a12fc9d710abefb5c25deb2e60e8
euler <kim25444@naver.com>
2024-07-31 14:08:55 +0900

    o modify: wrong file path(scripts/core.sh)

--- install.sh
+++ install.sh
@@ -7,12 +7,12 @@ if [ $CURRENT_SHELL = "bash" ]; then
 	if [ -f $HOME/.bashrc ]; then
 		mv $HOME/.bashrc $HOME/.bashrc.$(date +%Y%m%d%H%M%S);
 	fi;
-	ln -s $TOP_DIR/.core.sh $HOME/.bashrc;
+	ln -s $TOP_DIR/scripts/core.sh $HOME/.bashrc;
 elif [ $CURRENT_SHELL = "zsh" ]; then
 	if [ -f $HOME/.zshrc ]; then
 		mv $HOME/.zshrc $HOME/.zshrc.$(date +%Y%m%d%H%M%S);
 	fi;
-	ln -s $TOP_DIR/.core.sh $HOME/.zshrc;
+	ln -s $TOP_DIR/scripts/core.sh $HOME/.zshrc;
 else
 	echo "Unsupported shell: $CURRENT_SHELL";
 	exit 1;
